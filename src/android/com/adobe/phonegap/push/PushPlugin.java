5a788d3602b797daeec0b69fe4a3fb026c6a5759
Suphot <suphot@Suphots-MacBook-Pro.local>
2020-07-25 05:12:30 +0700

    import java.util.Set;

--- src/android/com/adobe/phonegap/push/PushPlugin.java
+++ src/android/com/adobe/phonegap/push/PushPlugin.java
@@ -34,6 +34,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import me.leolin.shortcutbadger.ShortcutBadger;
 
